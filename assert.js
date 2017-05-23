549150b66e8eac785c4f7392ad339fd3c9104f80
Peter Barcsak <peter.barcsak@gmail.com>
2017-05-23 14:40:36 +0100

    Added functionality to testing Framework: isEqualTO, isContain

--- assert.js
+++ assert.js
@@ -2,18 +2,28 @@ var parentElement = document.getElementById("div1");
 
 var assert = {
   isTrue: function(assertionToCheck, testDescription) {
-    if (!assertionToCheck) {
-      var node = document.createTextNode(`Fail: ${testDescription}`)
-      var para = document.createElement("p");
-      para.appendChild(node);
-      parentElement.appendChild(para);
+    result(assertionToCheck, testDescription)
+  },
 
-    } else {
-      var node = document.createTextNode(`Pass: ${testDescription}`)
-      var para = document.createElement("p");
-      para.appendChild(node);
-      parentElement.appendChild(para);
+  isEqualTo: function(a, b, testDescription) {
+    result((a === b), testDescription)
+  },
 
-    }
+  isContain: function(a, b, testDescription) {
+      result(a.includes(b), testDescription)
   }
+
+  }
+  function result(testFunction, testDescription) {
+    var result = (testFunction) ? "Pass" : "Fail";
+    printTestResult(result, testDescription)
+  }
+
+  function printTestResult(result, testDescription) {
+    // var node = document.createTextNode(`${result}: ${testDescription}`)
+    // var para = document.createElement("p")
+    // para.appendChild(node);
+    // parentElement.appendChild(para);
+    document.write("<p class=" + result + "> " + testDescription "</p>")
+
 };
--- noteTests.js
+++ noteTests.js
@@ -4,17 +4,32 @@
     assert.isTrue((1 + 1 === 2), "checks if 1 + 1 equals 2");
   };
 
-  function testIf5BiggerThan6() {
-    assert.isTrue((5 > 6), "checks if 5 is bigger than 6")
+  function testIf5BiggerThan6(a, b) {
+    assert.isTrue((a > b), "checks if 5 is bigger than 6")
   };
 
   function testIf3SmallerThan5() {
     assert.isTrue((3 < 5), "3 is smaller than 5")
   };
 
+  function testIf3Equal3() {
+    assert.isEqualTo(3, 3, "3 is equal to 3")
+  };
+
+  function arrayContainElement(a, b) {
+    assert.isContain(a, b, "Array of numbers contain 3")
+  }
+
+  function stringContainString(a, b) {
+    assert.isContain(a, b, "'String' contains 'tr'")
+  }
+
   testIf1Plus1Is2();
-  testIf5BiggerThan6();
-  testIf3SmallerThan5()
+  testIf5BiggerThan6(5, 6);
+  testIf3SmallerThan5();
+  testIf3Equal3();
+  arrayContainElement([1,2,3],3);
+  stringContainString("String", "tr")
 })(this);
 
 //
