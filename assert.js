fc5785c40254125d2f3721944125611b88156dc4
Peter Barcsak <peter.barcsak@gmail.com>
2017-05-23 15:49:56 +0100

    Adding colors to the tests

--- assert.js
+++ assert.js
@@ -20,10 +20,13 @@ var assert = {
   }
 
   function printTestResult(result, testDescription) {
-    // var node = document.createTextNode(`${result}: ${testDescription}`)
-    // var para = document.createElement("p")
-    // para.appendChild(node);
-    // parentElement.appendChild(para);
-    document.write("<p class=" + result + "> " + testDescription "</p>")
+    var color = (result === "Pass") ? "green" : "red"
+    var node = document.createTextNode(`${result}: ${testDescription}`)
+    var para = document.createElement("p")
+    // para.style.getElementByTagName("p")[0].setAttribute("class", "pass");
+    para.style.color = color
+    para.appendChild(node);
+    parentElement.appendChild(para);
+    // document.write("<p class=" + result + "> " + testDescription "</p>")
 
 };
